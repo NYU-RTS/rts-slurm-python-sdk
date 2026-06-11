40b87894bfcbe6f9f88f02fcf3e6a600952b06b2
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2026-06-11 17:41:58 -0400

    more trimming

--- slurm_rest_api_client/models/__init__.py
+++ slurm_rest_api_client/models/__init__.py
@@ -1,8 +1,5 @@
 """Contains all the data models used in inputs/outputs"""
 
-from .slurm_v0043_delete_job_flags import SlurmV0043DeleteJobFlags
-from .slurm_v0043_get_job_flags import SlurmV0043GetJobFlags
-from .slurm_v0043_get_jobs_flags import SlurmV0043GetJobsFlags
 from .slurmdb_v0043_post_users_association_flags import SlurmdbV0043PostUsersAssociationFlags
 from .v0043_account import V0043Account
 from .v0043_account_flags_item import V0043AccountFlagsItem
@@ -25,23 +22,6 @@ from .v0043_assoc_min import V0043AssocMin
 from .v0043_assoc_rec_set import V0043AssocRecSet
 from .v0043_assoc_short import V0043AssocShort
 from .v0043_coord import V0043Coord
-from .v0043_float_64_no_val_struct import V0043Float64NoValStruct
-from .v0043_job_info import V0043JobInfo
-from .v0043_job_info_flags_item import V0043JobInfoFlagsItem
-from .v0043_job_info_job_state_item import V0043JobInfoJobStateItem
-from .v0043_job_info_mail_type_item import V0043JobInfoMailTypeItem
-from .v0043_job_info_profile_item import V0043JobInfoProfileItem
-from .v0043_job_info_shared_item import V0043JobInfoSharedItem
-from .v0043_job_res import V0043JobRes
-from .v0043_job_res_core import V0043JobResCore
-from .v0043_job_res_core_status_item import V0043JobResCoreStatusItem
-from .v0043_job_res_node import V0043JobResNode
-from .v0043_job_res_node_cpus import V0043JobResNodeCpus
-from .v0043_job_res_node_memory import V0043JobResNodeMemory
-from .v0043_job_res_nodes import V0043JobResNodes
-from .v0043_job_res_nodes_select_type_item import V0043JobResNodesSelectTypeItem
-from .v0043_job_res_select_type_item import V0043JobResSelectTypeItem
-from .v0043_job_res_socket import V0043JobResSocket
 from .v0043_kill_jobs_msg import V0043KillJobsMsg
 from .v0043_kill_jobs_msg_flags_item import V0043KillJobsMsgFlagsItem
 from .v0043_kill_jobs_msg_job_state_item import V0043KillJobsMsgJobStateItem
@@ -53,8 +33,6 @@ from .v0043_openapi_accounts_resp import V0043OpenapiAccountsResp
 from .v0043_openapi_assocs_removed_resp import V0043OpenapiAssocsRemovedResp
 from .v0043_openapi_assocs_resp import V0043OpenapiAssocsResp
 from .v0043_openapi_error import V0043OpenapiError
-from .v0043_openapi_job_info_resp import V0043OpenapiJobInfoResp
-from .v0043_openapi_kill_job_resp import V0043OpenapiKillJobResp
 from .v0043_openapi_kill_jobs_resp import V0043OpenapiKillJobsResp
 from .v0043_openapi_meta import V0043OpenapiMeta
 from .v0043_openapi_meta_client import V0043OpenapiMetaClient
@@ -64,23 +42,14 @@ from .v0043_openapi_meta_slurm_version import V0043OpenapiMetaSlurmVersion
 from .v0043_openapi_users_add_cond_resp import V0043OpenapiUsersAddCondResp
 from .v0043_openapi_users_add_cond_resp_str import V0043OpenapiUsersAddCondRespStr
 from .v0043_openapi_warning import V0043OpenapiWarning
-from .v0043_part_prio import V0043PartPrio
-from .v0043_process_exit_code_verbose import V0043ProcessExitCodeVerbose
-from .v0043_process_exit_code_verbose_signal import V0043ProcessExitCodeVerboseSignal
-from .v0043_process_exit_code_verbose_status_item import V0043ProcessExitCodeVerboseStatusItem
 from .v0043_tres import V0043Tres
-from .v0043_uint_16_no_val_struct import V0043Uint16NoValStruct
 from .v0043_uint_32_no_val_struct import V0043Uint32NoValStruct
-from .v0043_uint_64_no_val_struct import V0043Uint64NoValStruct
 from .v0043_user_short import V0043UserShort
 from .v0043_user_short_adminlevel_item import V0043UserShortAdminlevelItem
 from .v0043_users_add_cond import V0043UsersAddCond
 
 __all__ = (
     "SlurmdbV0043PostUsersAssociationFlags",
-    "SlurmV0043DeleteJobFlags",
-    "SlurmV0043GetJobFlags",
-    "SlurmV0043GetJobsFlags",
     "V0043Account",
     "V0043AccountFlagsItem",
     "V0043Accounting",
@@ -102,23 +71,6 @@ __all__ = (
     "V0043AssocRecSet",
     "V0043AssocShort",
     "V0043Coord",
-    "V0043Float64NoValStruct",
-    "V0043JobInfo",
-    "V0043JobInfoFlagsItem",
-    "V0043JobInfoJobStateItem",
-    "V0043JobInfoMailTypeItem",
-    "V0043JobInfoProfileItem",
-    "V0043JobInfoSharedItem",
-    "V0043JobRes",
-    "V0043JobResCore",
-    "V0043JobResCoreStatusItem",
-    "V0043JobResNode",
-    "V0043JobResNodeCpus",
-    "V0043JobResNodeMemory",
-    "V0043JobResNodes",
-    "V0043JobResNodesSelectTypeItem",
-    "V0043JobResSelectTypeItem",
-    "V0043JobResSocket",
     "V0043KillJobsMsg",
     "V0043KillJobsMsgFlagsItem",
     "V0043KillJobsMsgJobStateItem",
@@ -130,8 +82,6 @@ __all__ = (
     "V0043OpenapiAssocsRemovedResp",
     "V0043OpenapiAssocsResp",
     "V0043OpenapiError",
-    "V0043OpenapiJobInfoResp",
-    "V0043OpenapiKillJobResp",
     "V0043OpenapiKillJobsResp",
     "V0043OpenapiMeta",
     "V0043OpenapiMetaClient",
@@ -141,14 +91,8 @@ __all__ = (
     "V0043OpenapiUsersAddCondResp",
     "V0043OpenapiUsersAddCondRespStr",
     "V0043OpenapiWarning",
-    "V0043PartPrio",
-    "V0043ProcessExitCodeVerbose",
-    "V0043ProcessExitCodeVerboseSignal",
-    "V0043ProcessExitCodeVerboseStatusItem",
     "V0043Tres",
-    "V0043Uint16NoValStruct",
     "V0043Uint32NoValStruct",
-    "V0043Uint64NoValStruct",
     "V0043UsersAddCond",
     "V0043UserShort",
     "V0043UserShortAdminlevelItem",
